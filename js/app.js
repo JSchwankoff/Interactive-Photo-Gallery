58439c6ee316ecec4ac51e8f64376a827e2b4d54
James Schwankoff <j.a.schwankoff@gmail.com>
2022-02-23 14:23:23 -0500

    added caption view for lightbox images

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
-baguetteBox.run('.gallery');
+baguetteBox.run('.gallery', {
+    captions: function(element) {
+        return element.getElementsByTagName('img')[0].alt;
+    }
+});
 const search = new Filter('search', 'data-caption');
